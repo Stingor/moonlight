852ae07336269c0d19fa243e220c0754b80eb258
Aleos <aleos89@users.noreply.github.com>
2021-10-05 23:01:32 -0400

    Caps the item and mob display name length (#6293)
    * Resolves an issue of trying to create an item or mob whose name was over the cap limit.
    * Display a warning now when the length is over the cap.
    * Resized the item and monster database values that were over the cap.
    Thanks to @Lemongrass3110, @secretdataz, and @Atemo
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/mob.cpp
+++ src/map/mob.cpp
@@ -4353,6 +4353,10 @@ uint64 MobDatabase::parseBodyNode(const YAML::Node &node) {
 		if (!this->asString(node, "AegisName", name))
 			return 0;
 
+		if (name.length() > NAME_LENGTH) {
+			this->invalidWarning(node["AegisName"], "AegisName \"%s\" exceeds maximum of %d characters, capping...\n", name.c_str(), NAME_LENGTH - 1);
+		}
+
 		name.resize(NAME_LENGTH);
 		mob->sprite = name;
 	}
@@ -4363,6 +4367,10 @@ uint64 MobDatabase::parseBodyNode(const YAML::Node &node) {
 		if (!this->asString(node, "Name", name))
 			return 0;
 
+		if (name.length() > NAME_LENGTH) {
+			this->invalidWarning(node["Name"], "Name \"%s\" exceeds maximum of %d characters, capping...\n", name.c_str(), NAME_LENGTH - 1);
+		}
+
 		name.resize(NAME_LENGTH);
 		mob->name = name;
 	}
@@ -4372,6 +4380,10 @@ uint64 MobDatabase::parseBodyNode(const YAML::Node &node) {
 
 		if (!this->asString(node, "JapaneseName", name))
 			return 0;
+
+		if (name.length() > NAME_LENGTH) {
+			this->invalidWarning(node["JapaneseName"], "JapaneseName \"%s\" exceeds maximum of %d characters, capping...\n", name.c_str(), NAME_LENGTH - 1);
+		}
 
 		name.resize(NAME_LENGTH);
 		mob->jname = name;
