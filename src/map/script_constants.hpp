832f3d4cef2573218e0a5767726067b2c7e41e7c
Atemo <Atemo@users.noreply.github.com>
2021-10-26 14:56:47 +0200

    Wave mode instances - walkthrough conversions (#3884)
    * Wave mode instances - walkthrough conversions
    
    * NPC_EMOTION and NPC_EMOTION_ON disrupted the walking system and have been restricted on the maps.
    * Implemented AI_SPECIALs. AI and normal monsters can naturally fight each others. However monsters with AI_SPECIAL can't be hit by normal monsters.
    
    * Implemented mob_setidleevent command.
    `mob_setidleevent <GID>,<event>;`
    This command will attach an event label to the monster with the given <GID> which will execute when the <GID> is idle.
    
    * Added parameters to unitskilluseid and unitskillusepos
    -- `<cancel>`: define if the skill can be interrupted when hit (by default the cancel value was 'castcancel' from skill_db.txt)
    -- `<Line_ID>` : the monster will say the message from 'Line_ID' in mob_chat_db.yml when casting the skill
    
    * Added `UMOB_IGNORE_CELL_STACK_LIMIT` for setunitdata/getunitdata script command.
    When true, the monster will ignore the stack limit (max number of characters that can stack within a single cell) defined by 'official_cell_stack_limit' in misc.conf
    
    * The script is disabled by default like on KRO
    
    Thanks to @Lemongrass3110 @aleos89 @Badarosk0 @sigtus @Questune09 !

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -3849,6 +3849,7 @@
 	export_constant(AI_LEGION);
 	export_constant(AI_FAW);
 	export_constant(AI_GUILD);
+	export_constant(AI_WAVEMODE);
 
 	/* battle flags */
 	export_constant(BF_NONE);
@@ -4382,6 +4383,7 @@
 	export_constant(UMOB_ROBE);
 	export_constant(UMOB_BODY2);
 	export_constant(UMOB_GROUP_ID);
+	export_constant(UMOB_IGNORE_CELL_STACK_LIMIT);
 
 	/* unit control - homunculus */
 	export_constant(UHOM_SIZE);
