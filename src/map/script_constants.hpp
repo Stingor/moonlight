4fa8a9b884578ab15f7d0cf9bde96bdc56b1cbe4
idk-whoami <60462533+idk-whoami@users.noreply.github.com>
2021-11-22 21:44:56 +0700

    Corrected script for some item (#6364)

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -5270,6 +5270,7 @@
 	export_constant(IG_EASTER_EGG);
 	export_constant(IG_PITAPAT_BOX);
 	export_constant(IG_HAPPY_BOX_J);
+	export_constant(IG_CLASS_SHADOW_CUBE);
 
 	/* unit stop walking */
 	export_constant(USW_NONE);
