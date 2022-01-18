5a5b2cb15c5631467d26937b29959b434bb734be
Lemongrass3110 <lemongrass@kstp.at>
2022-01-19 00:08:28 +0100

    Add more Github Actions (#6530)
    Renames "build_servers.yml" to "build_servers_gcc.yml"
    Removed Pre-Renewal and Renewal from GCC compilation
    Added an action for Pre-Renewal and Renewal
    Added an action for VIP
    Added an action for different packet versions
    Added MSVS build
    Changed some make server to make all
    Added master building
    Disabled LTO by default
    Added some missing override declarations
    Added Clang building
    Disabled Clang 12 and 13 for the time being
    
    Thanks to @aleos89 and @Akkarinage for their help and input.

--- src/map/itemdb.hpp
+++ src/map/itemdb.hpp
@@ -851,13 +851,13 @@ public:
 
 	}
 
-	void clear() {
+	void clear() override{
 		TypesafeYamlDatabase::clear();
 		this->combo_num = 0;
 	}
-	const std::string getDefaultLocation();
-	uint64 parseBodyNode(const YAML::Node& node);
-	void loadingFinished();
+	const std::string getDefaultLocation() override;
+	uint64 parseBodyNode(const YAML::Node& node) override;
+	void loadingFinished() override;
 };
 
 extern ComboDatabase itemdb_combo;
@@ -1036,9 +1036,9 @@ public:
 
 	}
 
-	const std::string getDefaultLocation();
-	uint64 parseBodyNode(const YAML::Node &node);
-	void loadingFinished();
+	const std::string getDefaultLocation() override;
+	uint64 parseBodyNode(const YAML::Node &node) override;
+	void loadingFinished() override;
 
 	// Additional
 	bool option_exists(std::string name);
@@ -1053,8 +1053,8 @@ public:
 
 	}
 
-	const std::string getDefaultLocation();
-	uint64 parseBodyNode(const YAML::Node &node);
+	const std::string getDefaultLocation() override;
+	uint64 parseBodyNode(const YAML::Node &node) override;
 
 	// Additional
 	bool add_option(const YAML::Node &node, std::shared_ptr<s_random_opt_group_entry> &entry);
@@ -1076,9 +1076,9 @@ public:
 
 	}
 
-	const std::string getDefaultLocation();
-	uint64 parseBodyNode(const YAML::Node& node);
-	void loadingFinished();
+	const std::string getDefaultLocation() override;
+	uint64 parseBodyNode(const YAML::Node& node) override;
+	void loadingFinished() override;
 	void clear() override{
 		TypesafeCachedYamlDatabase::clear();
 
@@ -1099,9 +1099,9 @@ public:
 
 	}
 
-	const std::string getDefaultLocation();
-	uint64 parseBodyNode(const YAML::Node& node);
-	void loadingFinished();
+	const std::string getDefaultLocation() override;
+	uint64 parseBodyNode(const YAML::Node& node) override;
+	void loadingFinished() override;
 
 	// Additional
 	bool item_exists(uint16 group_id, t_itemid nameid);
