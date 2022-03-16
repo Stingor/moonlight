ec6356a1819743647651474eef76136a6d68d7e9
Lemongrass3110 <lemongrass@kstp.at>
2022-03-16 16:46:48 +0100

    Fixed soul linking (#6697)
    Fixes #6696
    
    Thanks to @mazvi and @aleos89

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -10067,31 +10067,20 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 	case SL_STAR:
 	case SL_SUPERNOVICE:
 	case SL_WIZARD:
-	{
-		if (sd && tsc && (tsc->data[SC_SOULGOLEM] || tsc->data[SC_SOULSHADOW] || tsc->data[SC_SOULFALCON] || tsc->data[SC_SOULFAIRY])) { // Soul links from Soul Linker and Soul Reaper skills don't stack.
-			clif_skill_fail(sd, skill_id, USESKILL_FAIL,0);
-			break;
-		}
-		//NOTE: here, 'type' has the value of the associated MAPID, not of the SC_SPIRIT constant.
-		if (sd && dstsd && !((dstsd->class_&MAPID_UPPERMASK) == type)) {
-			clif_skill_fail(sd,skill_id,USESKILL_FAIL_LEVEL,0);
-			break;
-		}
 		if (skill_id == SL_SUPERNOVICE && dstsd && dstsd->die_counter && !(rnd()%100))
 		{	//Erase death count 1% of the casts
 			pc_setparam(dstsd, SP_PCDIECOUNTER, 0);
 			clif_specialeffect(bl, EF_ANGEL2, AREA);
 			//SC_SPIRIT invokes status_calc_pc for us.
 		}
-		// [Stingor] temps SPIRIT indexe sur le level du buffer pendant woe
-		int skilltime = skill_get_time(skill_id,skill_lv);
-		if( sd && is_agit_start() )
-			skilltime = (sd->status.base_level*580 < skilltime/3?skilltime/3:sd->status.base_level*580);
-
-		clif_skill_nodamage(src,bl,skill_id,skill_lv,
-			sc_start4(src,bl,SC_SPIRIT,100,skill_lv,skill_id,0,0,skilltime));
-
-		sc_start(src,src,SC_SMA,100,skill_lv,skill_get_time(SL_SMA,skill_lv));
+		if( sc_start2( src, bl, type, 100, skill_lv, skill_id, skill_get_time( skill_id, skill_lv ) ) ){
+			clif_skill_nodamage( src, bl, skill_id, skill_lv, 1 );
+			sc_start( src, src, SC_SMA, 100, skill_lv, skill_get_time( SL_SMA, skill_lv ) );
+		}else{
+			if( sd ){
+				clif_skill_fail( sd, skill_id, USESKILL_FAIL_LEVEL, 0 );
+			}
+		}
 		break;
 	}
 	case SL_HIGH:
@@ -10103,8 +10092,13 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 			clif_skill_fail(sd,skill_id,USESKILL_FAIL_LEVEL,0);
 			break;
 		}
+		// [Stingor] temps SPIRIT indexe sur le level du buffer pendant woe
+		int skilltime = skill_get_time(skill_id,skill_lv);
+		if( sd && is_agit_start() )
+			skilltime = (sd->status.base_level*580 < skilltime/3?skilltime/3:sd->status.base_level*580);
+
 		clif_skill_nodamage(src,bl,skill_id,skill_lv,
-			sc_start4(src,bl,type,100,skill_lv,skill_id,0,0,skill_get_time(skill_id,skill_lv)));
+			sc_start4(src,bl,type,100,skill_lv,skill_id,0,0,skilltime));
 		sc_start(src,src,SC_SMA,100,skill_lv,skill_get_time(SL_SMA,skill_lv));
 		break;
 	case SP_SOULGOLEM:
