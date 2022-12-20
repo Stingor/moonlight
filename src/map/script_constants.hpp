ad9531bddc8db8c1d5d1270a24df07c1b6481384
Aleos <aleos89@users.noreply.github.com>
2022-12-20 17:05:27 -0500

    Adds official Wand of Hermode clear list (#7494)
    * Wand of Hermode has a unique status clear list separate from the list other skills use.
    * Adds RemoveOnHermode status flag to identify statuses that can be removed from Wand of Hermode.
    Thanks to @mrjnumber1 and @Lemongrass3110!

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -9918,6 +9918,7 @@
 	export_constant(SCF_REMOVEONUNEQUIP);
 	export_constant(SCF_REMOVEONUNEQUIPWEAPON);
 	export_constant(SCF_REMOVEONUNEQUIPARMOR);
+	export_constant(SCF_REMOVEONHERMODE);
 
 	/* enchantgrades */
 	export_constant(ENCHANTGRADE_NONE);
