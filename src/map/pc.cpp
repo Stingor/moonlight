ce48f36c31a3830ad17837b1fa7395b2a2996d96
Playtester <3785983+Playtester@users.noreply.github.com>
2024-06-19 07:53:30 +0200

    Guillotine Fist SP Recovery Prevention Cleanup (#8443)
    - Unified code for SC_EXTREMITYFIST
      * It stops natural SP recovery for 300s in pre-re
      * It stops natural SP recovery and recovery from items for 3s and shows icon in renewal
    - Removed SC_EXTREMITYFIST2 as it's no longer needed
    - Fixed an issue that caused a natural SP regen tick to happen right after using Guillotine Fist
    - Fixes #8377

--- src/map/pc.cpp
+++ src/map/pc.cpp
@@ -10739,7 +10739,7 @@ int pc_itemheal(map_session_data *sd, t_itemid itemid, int hp, int sp)
 		}
 
 #ifdef RENEWAL
-		if (sd->sc.getSCE(SC_EXTREMITYFIST2))
+		if (sd->sc.getSCE(SC_EXTREMITYFIST))
 			sp = 0;
 #endif
 		if (sd->sc.getSCE(SC_BITESCAR))
