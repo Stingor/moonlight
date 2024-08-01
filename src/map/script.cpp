64ec3d47f82880bdc59fc3e1fe7c18ebb9f96b1e
eppc0330 <47050704+eppc0330@users.noreply.github.com>
2024-08-01 22:05:54 +0900

    Add mapflag specialpopup (#8365)
    * Add packet ZC_SPECIALPOPUP.
    * Add mapflag and script command specialpopup.
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>
    Co-authored-by: Atemo <Atemo@users.noreply.github.com>
    Co-authored-by: Aleos <aleos89@users.noreply.github.com>

--- src/map/script.cpp
+++ src/map/script.cpp
@@ -27236,6 +27236,22 @@ BUILDIN_FUNC(opentips){
 #endif
 }
 
+/**
+ * Displays a special popup.
+ * specialpopup(<popup id>);
+ */
+BUILDIN_FUNC(specialpopup) {
+	map_session_data* sd;
+
+	if( !script_rid2sd(sd) )
+		return SCRIPT_CMD_FAILURE;
+
+	int32 id = script_getnum(st,2);
+	clif_specialpopup(*sd, id);
+
+	return SCRIPT_CMD_SUCCESS;
+}
+
 BUILDIN_FUNC(setdialogalign){
 	map_session_data *sd;
 
@@ -28145,6 +28161,7 @@ struct script_function buildin_func[] = {
 	BUILDIN_DEF(has_autoloot,"?"),
 	BUILDIN_DEF(autoloot,"??"),
 	BUILDIN_DEF(opentips, "i?"),
+	BUILDIN_DEF(specialpopup,"i"),
 
 	BUILDIN_DEF(setdialogalign, "i"),
 	BUILDIN_DEF(setdialogsize, "ii"),
