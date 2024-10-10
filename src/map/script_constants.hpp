47b4b022ddf57136ff6b58d7759cbcb05c00a2df
Atemo <Atemo@users.noreply.github.com>
2024-10-10 14:46:16 +0200

    Implemented SC_EP16_DEF, RC2_EP16_DEF and corrected pre-renewal Acaraje item script (#8733)
    * Corrected pre-renewal Acaraje (id: 12375) item script
    * Implemented SC_EP16_DEF
    * Implemented RC2_EP16_DEF

--- src/map/script_constants.hpp
+++ src/map/script_constants.hpp
@@ -1922,6 +1922,7 @@
 	export_constant(SC_BATH_FOAM_B);
 	export_constant(SC_BATH_FOAM_C);
 	export_constant(SC_BUCHEDENOEL);
+	export_constant(SC_EP16_DEF);
 
 	/* status icons */
 	export_deprecated_constant2("SI_BLANK",-1);
@@ -4289,6 +4290,7 @@
 	export_constant(RC2_ILLUSION_LUANDA);
 	export_constant(RC2_ILLUSION_FROZEN);
 	export_constant(RC2_ILLUSION_MOONLIGHT);
+	export_constant(RC2_EP16_DEF);
 	export_constant(RC2_MAX);
 
 	/* monster ai */
