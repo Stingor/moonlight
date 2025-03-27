5b5ca22bdf8434dfcbeb790836277daacf29190e
Daegaladh <Daegaladh@users.noreply.github.com>
2025-03-27 14:53:32 +0100

    Follow up to 6e8a8597d91ccf68d5d911c220d8e41d93c713a2 (#9201)

--- src/map/pc.cpp
+++ src/map/pc.cpp
@@ -2976,7 +2976,7 @@ uint16 pc_getpercentweight(map_session_data& sd, uint32 weight)
 {
 	if (weight == 0)
 		weight = sd.weight;
-	return static_cast<uint16>(weight * 100 / sd.max_weight);
+	return static_cast<uint16>(weight * 100 / std::max<uint32>(sd.max_weight, 1));
 }
 
 /*==========================================
