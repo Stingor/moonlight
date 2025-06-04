0e48bfbb75eb69d447162ea7c822818c43be5ed0
Playtester <3785983+Playtester@users.noreply.github.com>
2025-06-04 23:22:50 +0200

    Official Wink Charm Implementation (#9389)
    - Wink Charm no longer spams the client with hundreds of emotions and move commands
    - Complete re-implementation of Wink Charm
      * Wink Charm blocks skill casting for non-players
      * Wink Charm blocks attacks on the caster for non-players
      * Wink Charm no longer makes the target move towards the caster, but can trigger aggro like a damage skill
      * Wink Charm can trigger additional effects just like a ranged weapon attack
      * Wink Charm can no longer target bosses, but bosses can now be affected by it (Aliza Card)
      * When Wink Charm fails due to invalid target, it will no longer consume SP nor set aftercast delay
      * When the target is valid, Wink Charm now always shows the animation, even if the status is resisted
      * When Wink Charm ends, the affected monster will drop its target
    - Wink Charm can still cause Confusion to players
      * Confusion has a 10% base chance in pre-re and a 100% base chance in renewal
      * Confusion now lasts 30s in pre-re and 18+2s in renewal
      * In pre-re, when Confusion succeeds, players will also do the heart emotion for 10 seconds
      * In renewal, Wink Charm causes Hallucination to players for 10 seconds instead (unavoidable)
    - Properly integrated SC_WINKCHARM and SC_VOICEOFSIREN into the interval status change system
      * Both skills show the heart emotion every 2250ms
    - Pang Voice shares the boss target block logic with Wink Charm
    - Fixes #9379

--- src/map/status.cpp
+++ src/map/status.cpp
@@ -2116,19 +2116,6 @@ bool status_check_skilluse(struct block_list *src, struct block_list *target, ui
 		)
 			return false;
 
-		if (sc->getSCE(SC_WINKCHARM) && target && !flag) { // Prevents skill usage
-			block_list *wink_target = map_id2bl(sc->getSCE(SC_WINKCHARM)->val2);
-
-			if (wink_target != nullptr) {
-				unit_data *wink_ud = unit_bl2ud(src);
-				if (wink_ud != nullptr && wink_ud->walktimer == INVALID_TIMER)
-					unit_walktobl(src, map_id2bl(sc->getSCE(SC_WINKCHARM)->val2), 3, 1);
-				clif_emotion( *src, ET_THROB );
-				return false;
-			} else
-				status_change_end(src, SC_WINKCHARM);
-		}
-
 		if (sc->getSCE(SC_BLADESTOP)) {
 			switch (sc->getSCE(SC_BLADESTOP)->val1) {
 				case 5:
@@ -5685,6 +5672,12 @@ void status_calc_state( block_list& bl, status_change& sc, std::shared_ptr<s_sta
 		status_calc_state_sub( bl, sc, start, scdb, sc.cant.cast, SCS_NOCAST, SCS_NOCASTCOND, []( block_list& bl, status_change& sc, bool& restriction, const sc_type type, const status_change_entry& sce ) -> bool {
 			// Check the specific conditions
 			switch( type ){
+				case SC_WINKCHARM:
+					if (bl.type != BL_PC) {
+						restriction = true;
+					}
+					break;
+
 				case SC_OBLIVIONCURSE:
 					if( sce.val3 == 1 ){
 						restriction = true;
@@ -9582,6 +9575,9 @@ static int32 status_get_sc_interval(enum sc_type type)
 		case SC_KILLING_AURA:
 		case SC_BOSSMAPINFO:
 			return 1000;
+		case SC_WINKCHARM:
+		case SC_VOICEOFSIREN:
+			return 2250;
 		case SC_BURNING:
 		case SC_PYREXIA:
 			return 3000;
@@ -11171,6 +11167,8 @@ static bool status_change_start_post_delay(block_list* src, block_list* bl, sc_t
 		case SC_BLEEDING:
 		case SC_BURNING:
 		case SC_KILLING_AURA:
+		case SC_WINKCHARM:
+		case SC_VOICEOFSIREN:
 			tick_time = status_get_sc_interval(type);
 			val4 = tick - tick_time; // Remaining time
 			break;
@@ -12071,10 +12069,6 @@ static bool status_change_start_post_delay(block_list* src, block_list* bl, sc_t
 			break;
 		case SC_HARMONIZE:
 			val2 = 5 + 5 * val1;
-			break;
-		case SC_VOICEOFSIREN:
-			val4 = tick / 2000;
-			tick_time = 2000; // [GodLesZ] tick time
 			break;
 		case SC_DEEPSLEEP:
 			val4 = tick / 2000;
@@ -13822,6 +13816,11 @@ int32 status_change_end( struct block_list* bl, enum sc_type type, int32 tid ){
 				}
 			}
 			break;
+		case SC_WINKCHARM:
+			if (bl->type == BL_MOB) {
+				mob_unlocktarget(static_cast<mob_data*>(bl), gettick());
+			}
+			break;
 		case SC_TENSIONRELAX:
 			if (sc && (sc->getSCE(SC_WEIGHT50) || sc->getSCE(SC_WEIGHT90)))
 				status_get_regen_data(bl)->state.overweight = true; // Add the overweight flag back
@@ -14720,11 +14719,10 @@ TIMER_FUNC(status_change_timer){
 		}
 		break;
 
+	case SC_WINKCHARM:
 	case SC_VOICEOFSIREN:
-		if( --(sce->val4) >= 0 ) {
+		if (sce->val4 >= 0) {
 			clif_emotion( *bl, ET_THROB );
-			sc_timer_next(2000 + tick);
-			return 0;
 		}
 		break;
 
