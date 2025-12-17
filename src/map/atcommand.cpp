20d6aeb3aa7c73c26ebbc90ec36dac65bdc74250
Rytech2 <rytech16@gmail.com>
2025-12-17 13:18:17 -0600

    Corrected Handling of 2nd Upper Expanded Jobs / Fixed Skill Tree Issues (#9661)
    * Corrected Handling of 2nd Upper Expanded Jobs / Fixed Skill Tree Issues
    
    *Corrected the MAPID's used for the 2nd upper expanded jobs.
    -Just because these jobs are on par with the 4th jobs doesn't mean all of them should be handled
    -as a 4th job. The MAPID mask system doesn't care about that stuff. It only cares where on the
    -job tree each job goes. Leaving gaps in the tree can cause issues.
    
    *Checks for jobs that can have traits will now use the defined pc_is_trait_job check.
    -This removes the traits dependence on the JOBL_FOURTH check which is needed due to not every
    -2nd upper expanded job having a 4th job. There's a mix of those jobs being 2nd, 3rd, and 4th
    -jobs on the job tree.
    
    *Fixed a number of issues with the pc_calc_skilltree_normalize_job_sub function.
    -Spirit Handler couldn't learn skills due to the Summoner being handled like a Novice job.
    -Summoner is a 1st job with many skills in its skill tree and is fixed to be handled as such.
    -Expanded Super Novice and Hyper Novice had a issue where they could learn skills gained from
    -their current job without putting any skill points into any of the skills from their
    -predecessor jobs. Finally, I fixed a incorrect mask used in the 3rd job level check.
    
    * Fixed equips not usable by Night Watch, Shinkiro/Shirauni, and Spirit Handler
    
    Fixed awakening potion not usable by Spirit Handler.
    
    Fixed #7039
    Fixed #8030
    Fixed #9635

--- src/map/atcommand.cpp
+++ src/map/atcommand.cpp
@@ -3045,7 +3045,7 @@ ACMD_FUNC(param)
 	if( stat < PARAM_POW ){
 		status = pc_getstat( sd, SP_STR + stat - PARAM_STR );
 	}else{
-		if( !( sd->class_ & JOBL_FOURTH ) ){
+		if( !pc_is_trait_job(sd->class_) ){
 			clif_displaymessage(fd, msg_txt(sd, 797)); // This command is unavailable to non - 4th class.
 			return -1;
 		}
@@ -3171,7 +3171,7 @@ ACMD_FUNC(trait_all) {
 	return -1;
 #endif
 
-	if( !( sd->class_ & JOBL_FOURTH ) ){
+	if( !pc_is_trait_job(sd->class_) ){
 		clif_displaymessage(fd, msg_txt(sd, 797)); // This command is unavailable to non - 4th class.
 		return -1;
 	}
@@ -11111,13 +11111,13 @@ ACMD_FUNC(clonestat) {
 		pc_resetstate(sd);
 		if (pc_has_permission(sd, PC_PERM_BYPASS_STAT_ONCLONE)) {
 			for (i = PARAM_STR; i < PARAM_MAX; i++) {
-				if (i >= PARAM_POW && !(sd->class_ & JOBL_FOURTH))
+				if (i >= PARAM_POW && !pc_is_trait_job(sd->class_))
 					continue;
 				max_status[i] = SHRT_MAX;
 			}
 		} else {
 			for (i = PARAM_STR; i < PARAM_MAX; i++) {
-				if (i >= PARAM_POW && sd->class_ & JOBL_FOURTH)
+				if (i >= PARAM_POW && pc_is_trait_job(sd->class_))
 					continue;
 				max_status[i] = pc_maxparameter(sd, static_cast<e_params>(i));
 			}
@@ -11147,7 +11147,7 @@ ACMD_FUNC(clonestat) {
 			clif_updatestatus(*sd, static_cast<_sp>( SP_USTR + i ) );
 		}
 
-		if (sd->class_ & JOBL_FOURTH) {
+		if (pc_is_trait_job(sd->class_)) {
 			clonestat_check(pow, PARAM_POW);
 			clonestat_check(sta, PARAM_STA);
 			clonestat_check(wis, PARAM_WIS);
