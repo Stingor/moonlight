464d9ac9cdf7d4b2d92cefb7db0ef81406e50cc0
Atemo <Atemo@users.noreply.github.com>
2026-02-05 14:03:37 +0100

    Split up Arch Bishop skills (#9774)

--- src/map/skills/acolyte/heal.cpp
+++ src/map/skills/acolyte/heal.cpp
@@ -20,8 +20,19 @@ void SkillHeal::castendNoDamageId(block_list *src, block_list *bl, uint16 skill_
 
 	int32 heal = skill_calc_heal(src, bl, getSkillId(), skill_lv, true);
 
-	if (status_isimmune(bl) || (dstmd && (status_get_class(bl) == MOBID_EMPERIUM || status_get_class_(bl) == CLASS_BATTLEFIELD)))
+	// if (status_isimmune(bl) || (dstmd && (status_get_class(bl) == MOBID_EMPERIUM || status_get_class_(bl) == CLASS_BATTLEFIELD)))
+		// heal = 0;
+
+	// [Stingor] -->
+	if (status_get_class(bl) == MOBID_EMPERIUM || status_get_class(bl) == MOBID_GUARDIAN_STONE1 || status_get_class(bl)== MOBID_GUARDIAN_STONE2) {
+		status_heal(bl,battle_config.emp_heal_hp,0,0);
+		clif_skill_nodamage (src, *bl, getSkillId(), battle_config.emp_heal_hp);
+		return;
+	}
+
+	if (status_get_class_(bl) == CLASS_BATTLEFIELD)
 		heal = 0;
+	// [Stingor] <--
 
 	if (tsc != nullptr && !tsc->empty())
 	{
@@ -44,9 +55,16 @@ void SkillHeal::castendNoDamageId(block_list *src, block_list *bl, uint16 skill_
 	}
 
 	status_change_end(bl, SC_BITESCAR);
-	clif_skill_nodamage(src, *bl, getSkillId(), heal);
 	if (tsc && tsc->getSCE(SC_AKAITSUKI) && heal)
 		heal = ~heal + 1;
+
+	// [Stingor] -->
+	if (status_isimmune(bl) && battle_config.healgtb)
+		heal /= 100 / status_isimmune(bl);
+	heal = (heal * battle_config.heal_rate)/100;
+	// [Stingor] <--
+
+	clif_skill_nodamage(src, *bl, getSkillId(), heal);
 	t_exp heal_get_jobexp = status_heal(bl, heal, 0, 0);
 
 	if (sd && dstsd && heal > 0 && sd != dstsd && battle_config.heal_exp > 0)
