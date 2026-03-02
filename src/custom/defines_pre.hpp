f61461baea58d941d6acdbb36829e9ec776a0f53
Stingor <philippe.gosset900@orange.Fr>
2026-03-02 20:38:41 +0100

    Allow tracking of /src/custom (remove ignore)
    Removed the commented and active entries that ignored /src/custom from .gitignore. This lets files under src/custom be tracked by Git instead of being excluded.

---
+++ src/custom/defines_pre.hpp
@@ -0,0 +1,17 @@
+// Copyright (c) rAthena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#ifndef CONFIG_CUSTOM_DEFINES_PRE_HPP
+#define CONFIG_CUSTOM_DEFINES_PRE_HPP
+
+/**
+ * rAthena configuration file (http://rathena.org)
+ * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
+ **/
+#define PACKET_OBFUSCATION_KEY1 0x22677205
+#define PACKET_OBFUSCATION_KEY2 0x5C847779
+#define PACKET_OBFUSCATION_KEY3 0x4C8F19FA
+#define PACKETVER 20180620
+
+
+#endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
