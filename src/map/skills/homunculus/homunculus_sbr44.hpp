f94db9042060f0575cdcaea1479ecfc63b0b8e29
Atemo <Atemo@users.noreply.github.com>
2026-03-09 14:42:31 +0100

    Add impl for skill_counter_additional_effect (#9908)
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/skills/homunculus/homunculus_sbr44.hpp
+++ src/map/skills/homunculus/homunculus_sbr44.hpp
@@ -5,12 +5,10 @@
 
 #include "../skill_impl.hpp"
 
-#include "map/battle.hpp"
-
-class SkillFullBuster : public WeaponSkillImpl {
+class SkillSBR44 : public WeaponSkillImpl {
 public:
-	SkillFullBuster();
+	SkillSBR44();
 
 	void applyCounterAdditionalEffects(block_list* src, block_list* target, uint16 skill_lv, t_tick tick, int32& attack_type) const override;
-	void calculateSkillRatio(const Damage *wd, const block_list *src, const block_list *target, uint16 skill_lv, int32 &base_skillratio, int32 mflag) const override;
+	void calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const override;
 };
