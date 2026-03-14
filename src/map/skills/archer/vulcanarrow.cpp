11e88dbb34fc0f189078bec109518f86936845b9
Atemo <Atemo@users.noreply.github.com>
2026-03-14 23:19:51 +0100

    Added modifyDamageData impl (#9910)
    * Added modifyDamageData to modify struct Damage data from skills
    
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/skills/archer/vulcanarrow.cpp
+++ src/map/skills/archer/vulcanarrow.cpp
@@ -18,3 +18,13 @@ void SkillVulcanArrow::calculateSkillRatio(const Damage *wd, const block_list *s
 	skillratio += 100 + 100 * skill_lv;
 #endif
 }
+
+void SkillVulcanArrow::modifyDamageData(Damage& dmg, const block_list& src, const block_list& target, uint16 skill_lv) const {
+	map_session_data* sd = BL_CAST(BL_PC, src);
+	if( battle_config.vulcanpvphit != 0 ) {
+		if( sd && map_flag_vs(sd->m) )
+			dmg.div_= battle_config.vulcanpvphit;
+		else
+			dmg.div_= skill_get_num( getSkillId(), skill_lv );
+	}
+}
