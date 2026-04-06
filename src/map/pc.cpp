c7be2a52cb452f702e35e68005a08d3ecfea4894
Stingor <philippe.gosset900@orange.Fr>
2026-04-06 12:30:23 +0200

    Ajuste la logique de calcul du max ASPD par classe
    Harmonise le calcul de la vitesse d'attaque maximale (ASPD) dans pc_maxaspd :
    - Les classes Clown, Gypsy, Super Novice, Taekwon et Lord Knight (Berserk) utilisent toujours 193.
    - Gunslinger et Assassin Cross utilisent 191 en PVP, sinon 193 en PVE.
    - Les autres classes utilisent 190 en PVP, sinon 193 en PVE.
    
    Clarifie et unifie la gestion des limites ASPD selon la classe et la carte.

--- src/map/pc.cpp
+++ src/map/pc.cpp
@@ -15374,11 +15374,11 @@ int16 pc_maxaspd( const map_session_data* sd ) {
 		sd->status.class_ == JOB_SUPER_NOVICE ||
 		sd->status.class_ == JOB_TAEKWON ||
 		(sd->sc.getSCE(SC_BERSERK) && (sd->status.class_ == JOB_LORD_KNIGHT || sd->status.class_ == JOB_LORD_KNIGHT2)))
-		return (map_flag_vs(sd->m) ? battle_config.max_third_aspd : battle_config.max_extended_aspd) - aspd2;
+		return battle_config.max_extended_aspd - aspd2;
 	else if (sd->status.class_ == JOB_GUNSLINGER || sd->status.class_ == JOB_ASSASSIN_CROSS)
-		return (map_flag_vs(sd->m) ? battle_config.max_aspd : battle_config.max_third_aspd) - aspd2;
+		return (map_flag_vs(sd->m) ? battle_config.max_third_aspd : battle_config.max_extended_aspd) - aspd2;
 	else
-		return battle_config.max_aspd - aspd2;
+		return (map_flag_vs(sd->m) ? battle_config.max_aspd : battle_config.max_extended_aspd) - aspd2;
 	//  <-- [Stingor]
 
 	return (( sd->class_&JOBL_THIRD) ? battle_config.max_third_aspd : (
