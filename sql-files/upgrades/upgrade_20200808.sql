cef6efd4a0cd5d238edcae62f75e164e9e99886f
Stingor <philippe.gosset900@orange.Fr>
2026-04-07 21:38:54 +0200

    Mise à jour des ALTER TABLE sur les tables de stockage
    Suppression des modifications sur les tables :
    - item_cash_db, item_cash_db2, item_db, item_db_re,
      item_db2, item_db2_re, mob_db, mob_db_re,
      mob_db2, mob_db2_re
    
    Ajout des ALTER TABLE pour :
    - storagealt1, storagealt2, storagealt3, storagealt4, storagecard
    
    Modification des colonnes :
    - nameid, card0, card1, card2, card3
    Type : int(10) unsigned NOT NULL default '0'

--- sql-files/upgrades/upgrade_20200808.sql
+++ sql-files/upgrades/upgrade_20200808.sql
@@ -60,80 +60,34 @@ ALTER TABLE `storage`
 	MODIFY `card2` int(10) unsigned NOT NULL default '0',
 	MODIFY `card3` int(10) unsigned NOT NULL default '0';
 
-ALTER TABLE `item_cash_db`
-	MODIFY `item_id` int(10) unsigned NOT NULL default '0';
-
-ALTER TABLE `item_cash_db2`
-	MODIFY `item_id` int(10) unsigned NOT NULL default '0';
-
-ALTER TABLE `item_db`
-	MODIFY `id` int(10) unsigned NOT NULL default '0';
-
-ALTER TABLE `item_db_re`
-	MODIFY `id` int(10) unsigned NOT NULL default '0';
-
-ALTER TABLE `item_db2`
-	MODIFY `id` int(10) unsigned NOT NULL default '0';
-
-ALTER TABLE `item_db2_re`
-	MODIFY `id` int(10) unsigned NOT NULL default '0';
-
-ALTER TABLE `mob_db`
-	MODIFY `MVP1id` int(10) unsigned NOT NULL default '0',
-	MODIFY `MVP2id` int(10) unsigned NOT NULL default '0',
-	MODIFY `MVP3id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop1id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop2id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop3id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop4id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop5id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop6id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop7id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop8id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop9id` int(10) unsigned NOT NULL default '0',
-	MODIFY `DropCardid` int(10) unsigned NOT NULL default '0';
-
-ALTER TABLE `mob_db_re`
-	MODIFY `MVP1id` int(10) unsigned NOT NULL default '0',
-	MODIFY `MVP2id` int(10) unsigned NOT NULL default '0',
-	MODIFY `MVP3id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop1id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop2id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop3id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop4id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop5id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop6id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop7id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop8id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop9id` int(10) unsigned NOT NULL default '0',
-	MODIFY `DropCardid` int(10) unsigned NOT NULL default '0';
-
-ALTER TABLE `mob_db2`
-	MODIFY `MVP1id` int(10) unsigned NOT NULL default '0',
-	MODIFY `MVP2id` int(10) unsigned NOT NULL default '0',
-	MODIFY `MVP3id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop1id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop2id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop3id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop4id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop5id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop6id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop7id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop8id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop9id` int(10) unsigned NOT NULL default '0',
-	MODIFY `DropCardid` int(10) unsigned NOT NULL default '0';
-
-ALTER TABLE `mob_db2_re`
-	MODIFY `MVP1id` int(10) unsigned NOT NULL default '0',
-	MODIFY `MVP2id` int(10) unsigned NOT NULL default '0',
-	MODIFY `MVP3id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop1id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop2id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop3id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop4id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop5id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop6id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop7id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop8id` int(10) unsigned NOT NULL default '0',
-	MODIFY `Drop9id` int(10) unsigned NOT NULL default '0',
-	MODIFY `DropCardid` int(10) unsigned NOT NULL default '0';
+ALTER TABLE `storagealt1`
+	MODIFY `nameid` int(10) unsigned NOT NULL default '0',
+	MODIFY `card0` int(10) unsigned NOT NULL default '0',
+	MODIFY `card1` int(10) unsigned NOT NULL default '0',
+	MODIFY `card2` int(10) unsigned NOT NULL default '0',
+	MODIFY `card3` int(10) unsigned NOT NULL default '0';
+ALTER TABLE `storagealt2`
+	MODIFY `nameid` int(10) unsigned NOT NULL default '0',
+	MODIFY `card0` int(10) unsigned NOT NULL default '0',
+	MODIFY `card1` int(10) unsigned NOT NULL default '0',
+	MODIFY `card2` int(10) unsigned NOT NULL default '0',
+	MODIFY `card3` int(10) unsigned NOT NULL default '0';
+ALTER TABLE `storagealt3`
+	MODIFY `nameid` int(10) unsigned NOT NULL default '0',
+	MODIFY `card0` int(10) unsigned NOT NULL default '0',
+	MODIFY `card1` int(10) unsigned NOT NULL default '0',
+	MODIFY `card2` int(10) unsigned NOT NULL default '0',
+	MODIFY `card3` int(10) unsigned NOT NULL default '0';
+ALTER TABLE `storagealt4`
+	MODIFY `nameid` int(10) unsigned NOT NULL default '0',
+	MODIFY `card0` int(10) unsigned NOT NULL default '0',
+	MODIFY `card1` int(10) unsigned NOT NULL default '0',
+	MODIFY `card2` int(10) unsigned NOT NULL default '0',
+	MODIFY `card3` int(10) unsigned NOT NULL default '0';
+	
+ALTER TABLE `storagecard`
+	MODIFY `nameid` int(10) unsigned NOT NULL default '0',
+	MODIFY `card0` int(10) unsigned NOT NULL default '0',
+	MODIFY `card1` int(10) unsigned NOT NULL default '0',
+	MODIFY `card2` int(10) unsigned NOT NULL default '0',
+	MODIFY `card3` int(10) unsigned NOT NULL default '0';
